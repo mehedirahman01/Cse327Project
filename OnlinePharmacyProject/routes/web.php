3772f3d2606adc8f5eac30ce51b51eb909f9427a
sourovhalder <59764662+SourovHalder@users.noreply.github.com>
2021-01-03 01:54:20 +0600

    merging resolved

--- OnlinePharmacyProject/routes/web.php
+++ OnlinePharmacyProject/routes/web.php
@@ -29,14 +29,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-=======
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
->>>>>>> master
