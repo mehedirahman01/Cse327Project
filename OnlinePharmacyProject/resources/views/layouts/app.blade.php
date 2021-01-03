cd4e47817656ba6670943cb7cd240d07dac74010
Mehedi Rahman <mehedi.rahman01@northsouth.edu>
2021-01-04 04:00:18 +0600

    updated from master

--- OnlinePharmacyProject/resources/views/layouts/app.blade.php
+++ OnlinePharmacyProject/resources/views/layouts/app.blade.php
@@ -1,36 +1,80 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
+<head>
+  <title>Online pharmacy</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  </head>
+  <style>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    /* Remove the navbar's default rounded borders and increase the bottom margin */
+    .navbar {
+      margin-bottom: 50px;
+      border-radius: 0;
+    }
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    /* Remove the jumbotron's default bottom margin */
+     .jumbotron {
+      margin-bottom: 0;
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    }
+    .carousel-inner img {
+      width: 100%; /* Set width to 100% */
+      margin: auto;
+      min-height:200px;
+  }
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
+  .dropdown {
+    position: relative;
+    display: inline-block;
+  }
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {background-color: #ddd;}
+
+.dropdown:hover .dropdown-content {display: block;}
+
+.dropdown:hover .dropbtn {background-color: #3e8e41;}
+
+  /* Hide the carousel text when the screen is less than 600 pixels wide */
+  @media (max-width: 600px) {
+    .carousel-caption {
+      display: none;
+    }
+
+}
+
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 50px;
+    }
+  </style>
+
+<body>
+
+
+@yield('content')
+
+</body>
 </html>
--- OnlinePharmacyProject/resources/views/welcome.blade.php
+++ OnlinePharmacyProject/resources/views/welcome.blade.php
@@ -1,10 +1,151 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    LARAVEL 2020
-  </body>
+@extends('layouts.app')
+@section('content')
+
+
+
+<nav class="navbar navbar-inverse" >
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Online Pharmacy</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Deals</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+
+      <form class="navbar-form navbar-right" action="">
+  <div class="input-group">
+    <input type="text" class="form-control" placeholder="Search medicines">
+    <div class="input-group-btn">
+      <button class="btn btn-default" type="submit">
+        <i class="glyphicon glyphicon-search"></i>
+      </button>
+    </div>
+  </div>
+</form>
+      @csrf
+      <ul class="nav navbar-nav navbar-right">
+        @auth
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Hello {{ Auth::user()->firstName }}</a></li>
+        @endauth
+        <li class="dropdown"><a href="#"><span class="glyphicon glyphicon-user"></span> Your Account</a>
+          <div class="dropdown-content" aria-label="submenu">
+            @auth
+            <a href="{{ route('logout') }}"> {{ __('Logout') }}</a>
+            @endauth
+            @guest
+            <a href="{{ route('new-register') }}">Register</a>
+            <a href="{{ route('new-login') }}">Login</a>
+            @endguest
+          </div>
+        </li>
+        <li><a href="#" onclick="return false";><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="/images/med2.jpg" style="width: 50%; height: 60%;" alt="Image">
+        <div class="carousel-caption">
+          <h3>get your daily need medicines at 10% discount</h3>
+          <p>Save Money</p>
+        </div>
+      </div>
+      <div class="item">
+        <img src="/images/med.jpg" style="width: 50%; height: 60%" alt="Image">
+
+        <div class="carousel-caption">
+          <h3>Get Vitamins at 25% discount</h3>
+          <p></p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="/images/doctor.jpg" style="width: 50%; height: 60%" alt="Image">
+
+        <div class="carousel-caption">
+          <h3>Need Doctors? </h3>
+          <p>Coming soon..</p>
+        </div>
+      </div>
+
+
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+</div>
+
+<br>
+<br>
+<br>
+<div class="container">
+
+
+  <div class="row">
+
+
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Our top selling products</div>
+        <div class="panel-body"><img src="/images/med3.jpg" class="img-responsive" style="width:100%; height:30%" alt="Image"></div>
+
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="panel panel-danger">
+        <div class="panel-heading">Best Deal</div>
+        <div class="panel-body"><img src="/images/med4.jpg"" class="img-responsive" style="width:100%; height:30%" alt="Image"></div>
+
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="panel panel-success">
+        <div class="panel-heading">Offered products</div>
+        <div class="panel-body"><img src="/images/med5.jpg"" class="img-responsive" style="width:100%; height:30%" alt="Image"></div>
+
+      </div>
+    </div>
+  </div>
+</div><br>
+<br>
+<br>
+<br>
+
+
+<footer class="container-fluid text-center">
+  <p>Online Pharmacy Copyright</p>
+
+</footer>
+<br>
+<br>
+
+
+</body>
 </html>
